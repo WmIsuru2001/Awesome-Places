3c8fe29640c2eef202e3ab5fa4f03b077572c83b
WmIsuru2001 <155750111+WmIsuru2001@users.noreply.github.com>
2025-04-21 23:01:50 +0530

    booking update

--- lib/pages/booking_page/bookings_form.dart
+++ lib/pages/booking_page/bookings_form.dart
@@ -1,3 +1,5 @@
+import 'package:awesome_places/utils/colors.dart';
+import 'package:awesome_places/widgets/shared/buttons.dart';
 import 'package:awesome_places/widgets/shared/inputs.dart';
 import 'package:flutter/material.dart';
 
@@ -6,32 +8,91 @@ class BookingsForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           "User Name",
           style: TextStyle(
             fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
-        Inputs(),
-        SizedBox(
+        const Inputs(),
+        const SizedBox(
           height: 10,
         ),
-        Text(
-          "User Name",
+        const Text(
+          "Country",
           style: TextStyle(
             fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
-        Inputs(),
+        const Inputs(),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "Team Size",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: mainColor,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const Center(
+                child: Text(
+                  "3",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: mainWhite,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "datWelcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you're seeking the tranquility visit offers something for every traveler.a",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButtons(
+              buttonText: "Submit",
+              buttonBgColor: starColor1,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 30,
+        ),
       ],
     );
   }
